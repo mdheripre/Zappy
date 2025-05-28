f6dcfe574758bb49ccc957fb8cf10547bdabaf40
NicoDumetz <nicolas.dumetz@epitech.eu>
2025-05-28 18:49:57 +0200

    [feat/Server] add object List

---
+++ server/include/list.h
@@ -0,0 +1,43 @@
+/*
+** EPITECH PROJECT, 2025
+** B-YEP-400-LIL-4-1-zappy-nicolas.dumetz
+** File description:
+** list
+*/
+
+
+#ifndef LIST_H_
+    #define LIST_H_
+
+    #include <stdlib.h>
+
+typedef struct list_node_s {
+    void *data;
+    struct list_node_s *next;
+} list_node_t;
+
+typedef struct list_s list_t;
+
+typedef struct list_methods_s {
+    void (*destroy)(list_t *self);
+    void (*clear)(list_t *self);
+    void (*push_back)(list_t *self, void *data);
+    void *(*pop_front)(list_t *self);
+    int (*size)(const list_t *self);
+} list_methods_t;
+
+struct list_s {
+    list_node_t *head;
+    list_node_t *tail;
+    int size;
+    void (*free_fn)(void *);
+    const list_methods_t *methods;
+};
+
+list_t *list_create(void (*free_fn)(void *));
+void list_destroy(list_t *self);
+void list_clear(list_t *self);
+void list_push_back(list_t *self, void *data);
+void *list_pop_front(list_t *self);
+int list_size(const list_t *self);
+#endif /* !LIST_H_ */
---
+++ server/src/List/methods/list_clear.c
@@ -0,0 +1,33 @@
+/*
+** EPITECH PROJECT, 2025
+** server
+** File description:
+** list_clear
+*/
+
+#include "list.h"
+
+/**
+ * @brief Clears all elements from the list and frees associated memory.
+ *
+ * Iterates through the list, freeing each node and its data using the
+ * provided free function (if any). Resets the list to an empty state.
+ *
+ * @param self Pointer to the list to clear.
+ */
+void list_clear(list_t *self)
+{
+    list_node_t *current = self->head;
+    list_node_t *next = NULL;
+
+    while (current) {
+        next = current->next;
+        if (self->free_fn)
+            self->free_fn(current->data);
+        free(current);
+        current = next;
+    }
+    self->head = NULL;
+    self->tail = NULL;
+    self->size = 0;
+}
