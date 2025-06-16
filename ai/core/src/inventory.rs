b6268b9a2547d14207e1d9201d14e2976144bdf2
Enzo Dubeaurepaire <enzo.dubeaurepaire@epitech.eu>
2025-06-16 16:07:07 +0200

    [refactor/ai_basic_algo] inventory now has its own struct

---
+++ ai/core/src/inventory.rs
@@ -0,0 +1,72 @@
+use crate::item::Item;
+
+#[derive(Clone, Debug)]
+pub struct Inventory {
+    pub food: usize,
+    pub linemate: usize,
+    pub deraumere: usize,
+    pub sibur: usize,
+    pub mendiane: usize,
+    pub phiras: usize,
+    pub thystame: usize,
+}
+
+impl Inventory {
+    pub fn new() -> Self {
+        Inventory {
+            food: 0,
+            linemate: 0,
+            deraumere: 0,
+            sibur: 0,
+            mendiane: 0,
+            phiras: 0,
+            thystame: 0,
+        }
+    }
+    
+    pub fn add_item(&mut self, item: &Item) {
+        match item {
+            Item::Food => self.food += 1,
+            Item::Linemate => self.linemate += 1,
+            Item::Deraumere => self.deraumere += 1,
+            Item::Sibur => self.sibur += 1,
+            Item::Mendiane => self.mendiane += 1,
+            Item::Phiras => self.phiras += 1,
+            Item::Thystame => self.thystame += 1,
+        }
+    }
+
+    pub fn remove_item(&mut self, item: &Item) -> Result<(), String> {
+        match item {
+            Item::Food if self.food > 0 => {
+                self.food -= 1;
+                Ok(())
+            }
+            Item::Linemate if self.linemate > 0 => {
+                self.linemate -= 1;
+                Ok(())
+            }
+            Item::Deraumere if self.deraumere > 0 => {
+                self.deraumere -= 1;
+                Ok(())
+            }
+            Item::Sibur if self.sibur > 0 => {
+                self.sibur -= 1;
+                Ok(())
+            }
+            Item::Mendiane if self.mendiane > 0 => {
+                self.mendiane -= 1;
+                Ok(())
+            }
+            Item::Phiras if self.phiras > 0 => {
+                self.phiras -= 1;
+                Ok(())
+            }
+            Item::Thystame if self.thystame > 0 => {
+                self.thystame -= 1;
+                Ok(())
+            }
+            _ => Err(format!("Cannot remove item: {:?}", item)),
+        }
+    }
+}
