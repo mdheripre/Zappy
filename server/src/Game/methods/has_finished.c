ef048a701367486bcb6e1eb28a6adeeefe9a9245
NicoDumetz <nicolas.dumetz@epitech.eu>
2025-06-17 17:56:01 +0200

    [fix/Server] overflow tick

--- server/src/Game/methods/has_finished.c
+++ server/src/Game/methods/has_finished.c
@@ -14,6 +14,15 @@
 /*                                                                          */
 /****************************************************************************/
 
+/**
+ * @brief Check if the game has reached a winning condition or overflow.
+ *
+ * A team wins if it has at least 6 players at the maximum level.
+ * Also ends the game if the tick counter overflows.
+ *
+ * @param self Pointer to the game instance.
+ * @return true if the game is finished, false otherwise.
+ */
 bool has_finished(game_t *self)
 {
     team_info_t *team;
@@ -22,6 +31,10 @@ bool has_finished(game_t *self)
         team = node->data;
         if (team && team->max_level_players >= 6)
             return true;
+    }
+    if (self->tick_counter >= ULONG_MAX) {
+        console_log(LOG_ERROR, "Tick counter overflow");
+        return true;
     }
     return false;
 }
