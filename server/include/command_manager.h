5bbf7e22a3b56b52bee11bb13d44b675e032f4d8
NicoDumetz <nicolas.dumetz@epitech.eu>
2025-06-17 19:28:29 +0200

    [feat/Server] add smg gui command

--- server/include/command_manager.h
+++ server/include/command_manager.h
@@ -61,6 +61,7 @@ void handle_gui_pnw(void *ctx, void *data);
 void handle_gui_suc(void *ctx, void *);
 void handle_gui_sbp(void *ctx, void *);
 void handle_gui_enw(void *ctx, void *data);
+void handle_gui_smg(void *ctx, void *data);
 
 /* Responses */
 void on_response_player_moved(void *ctx, void *data);
