11a8a592734079c07d95ec3be87108710d3dbedd
Enzo Dubeaurepaire <enzo.dubeaurepaire@epitech.eu>
2025-06-25 21:34:58 +0200

    [style/ai_role_system] made ai_decision more readable

--- ai/core/src/ai.rs
+++ ai/core/src/ai.rs
@@ -1,6 +1,8 @@
 use crate::{ai_state::AiState, broadcast::Message, broadcast::MessageType, item::Item, packet::Packet, Result};
 use std::{env, process::Stdio, sync::Arc, time::Duration};
 use tokio::{process::Command, sync::Mutex};
+use tokio::sync::MutexGuard;
+use crate::tile::Tile;
 
 /// Possible AI command to the server
 ///
@@ -107,21 +109,29 @@ pub async fn spawn_child_process() -> Result<()> {
 pub async fn ai_decision(state: &Arc<Mutex<AiState>>) -> Option<AiCommand> {
     let mut state = state.lock().await;
 
-    tokio::time::sleep(Duration::from_millis(500)).await;
-
     if state.last_command().is_some() {
         return None;
     }
-    if state.time() == 0 {
-        if !state.broadcast().get_received_messages().is_empty() {
-            state.broadcast().clear();
-        }
-        let msg = Message::new(*state.client_num() as u32, *state.message_id(), MessageType::Hello, None);
-        state.inc_time();
-        *state.last_command() = Some(AiCommand::Broadcast(msg.to_string()));
-        state.broadcast().send_message(msg.clone());
-        return Some(AiCommand::Broadcast(msg.to_string()));
+    update_destination(&mut state);
+    if let Some(command) = send_hello(&mut state)
+        .or_else(|| interpret_broadcast(&mut state))
+        .or_else(|| {
+            state.destination().clone()
+                .and_then(|dest| get_command_to_destination(&mut state, dest))
+        })
+        .or_else(|| look_or_forward(&mut state)) {
+        return forward_command(&mut state, Some(command));
     }
+    None
+}
+
+pub fn forward_command(state: &mut MutexGuard<'_, AiState>, command: Option<AiCommand>) -> Option<AiCommand> {
+    *state.last_command() = command.clone();
+    state.inc_time();
+    command
+}
+
+pub fn interpret_broadcast(state: &mut MutexGuard<'_, AiState>) -> Option<AiCommand> {
     while !state.broadcast().get_received_messages().is_empty() {
         if let Some(msg) = state.broadcast().pop_received() {
             match msg.msg_type() {
@@ -132,8 +142,7 @@ pub async fn ai_decision(state: &Arc<Mutex<AiState>>) -> Option<AiCommand> {
                         str.push(':');
                         str.push_str(state.team_inventory().as_broadcast().as_str());
                         let output_msg = Message::new(*state.client_num() as u32, *state.message_id(), MessageType::Welcome, Some(str));
-                        *state.last_command() = Some(AiCommand::Broadcast(output_msg.to_string()));
-                        return Some(AiCommand::Broadcast(output_msg.to_string()));
+                        return forward_command(state, Some(AiCommand::Broadcast(output_msg.to_string())))
                     } else {
                         *state.id_getter_queue() += 1;
                     }
@@ -141,7 +150,6 @@ pub async fn ai_decision(state: &Arc<Mutex<AiState>>) -> Option<AiCommand> {
                 MessageType::Welcome => {
                     if !*state.welcomed() && *state.id_getter_queue() == 0 {
                         if *state.alpha() && state.time() <= 10 {
-                            println!("Now a beta");
                             *state.alpha() = false;
                         }
                         if let Some(content) = msg.content() {
@@ -165,36 +173,48 @@ pub async fn ai_decision(state: &Arc<Mutex<AiState>>) -> Option<AiCommand> {
             }
         }
     }
-    state.inc_time();
+    None
+}
+
+pub fn send_hello(state: &mut MutexGuard<'_, AiState>) -> Option<AiCommand> {
+    if state.time() == 0 {
+        if !state.broadcast().get_received_messages().is_empty() {
+            state.broadcast().clear();
+        }
+        let msg = Message::new(*state.client_num() as u32, *state.message_id(), MessageType::Hello, None);
+        state.inc_time();
+        *state.last_command() = Some(AiCommand::Broadcast(msg.to_string()));
+        state.broadcast().send_message(msg.clone());
+        return Some(AiCommand::Broadcast(msg.to_string()))
+    }
+    None
+}
+
+pub fn get_command_to_destination(state: &mut MutexGuard<'_, AiState>, dest: Tile) -> Option<AiCommand> {
+    let dest_diff = dest.distance_as_pair(state.position());
+    if dest_diff.0 == 0 && dest_diff.1 == 0 {
+        if dest.nb_items <= 1 {
+            *state.destination() = None;
+        }
+        let command = state.chose_best_item(dest.get_items().keys().cloned().collect());
+        return forward_command(state, command);
+    }
+    if !state.direction().is_along(dest_diff) {
+        let command = Some(state.direction().get_best_turn_to(dest_diff));
+        return forward_command(state, command);
+    }
+    forward_command(state, Some(AiCommand::Forward))
+}
+
+pub fn update_destination(state: &mut MutexGuard<'_, AiState>) {
     if state.is_there_things_in_map() && state.destination().is_none() {
         *state.destination() = state.chose_destination_tile()
     }
-    if let Some(dest) = state.destination().clone() {
-        let dest_diff = dest.distance_as_pair(state.position());
-        if dest_diff.0 == 0 && dest_diff.1 == 0 {
-            if dest.nb_items <= 1 {
-                *state.destination() = None;
-            }
-            let command = state.chose_best_item(dest.get_items().keys().cloned().collect());
-            *state.last_command() = command.clone();
-            return command;
-        }
-        if !state.direction().is_along(dest_diff) {
-            let command = Some(state.direction().get_best_turn_to(dest_diff));
-            *state.last_command() = command.clone();
-            return command;
-        }
-        *state.last_command() = Some(AiCommand::Forward);
-        return Some(AiCommand::Forward);
-    }
+}
+
+pub fn look_or_forward(state: &mut MutexGuard<'_, AiState>) -> Option<AiCommand> {
     match state.previous_command() {
-        Some(AiCommand::Look) => {
-            *state.last_command() = Some(AiCommand::Forward);
-            Some(AiCommand::Forward)
-        }
-        _ => {
-            *state.last_command() = Some(AiCommand::Look);
-            Some(AiCommand::Look)
-        }
+        Some(AiCommand::Look) => forward_command(state, Some(AiCommand::Forward)),
+        _ => forward_command(state, Some(AiCommand::Look)),
     }
 }
